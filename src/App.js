c817ef4e74b23dec3e0168afb3c7a4011a5fc6af
mkocakoc <mkocakocfastking@gmail.com>
2024-02-15 01:04:07 +0300

    Working steps components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+const message =  [
+  "Step 1 , world!",
+  "Step 2: Learn React",
+  "Awesome! You're ready to learn React!"
+];
 
-function App() {
+export default function App() {
+  const steps = 1;
+
+  function previusFnc() {
+    steps > 1 ? steps - 1 : steps;
+  }
+
+  function nextFnc() {
+    steps < 3 ? steps + 1 : steps;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${steps >=  1 ?   'active': "" }`}>1</div>
+        <div className={`${steps >=  2 ?   'active': "" }`}>2</div>
+        <div className={`${steps >=  3 ?   'active': "" }`}>3</div>
+      </div>
+
+      <p className="message">Steps {steps} : {message [steps - 1]} !</p>
+      <div className="buttons">
+        <button style={{ background: "#7950f2", color: "#fff" }} onClick={previusFnc}>Prev</button>
+        <button style={{ background: "#7950f2", color: "#fff" }} onClick={nextFnc}>Next</button>
+      </div>
     </div>
   );
 }
-
-export default App;
